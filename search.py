dc494c9c16d8e6a480ccdbf80b4472bec822d190
katherineh300 <44074586+katherineh300@users.noreply.github.com>
2022-01-31 17:38:23 -0600

    added A*

--- search.py
+++ search.py
@@ -26,7 +26,6 @@ class SearchProblem:
     """
     This class outlines the structure of a search problem, but doesn't implement
     any of the methods (in object-oriented terminology: an abstract class).
-
     You do not need to change anything in this class, ever.
     """
 
@@ -39,7 +38,6 @@ class SearchProblem:
     def isGoalState(self, state):
         """
           state: Search state
-
         Returns True if and only if the state is a valid goal state.
         """
         util.raiseNotDefined()
@@ -47,7 +45,6 @@ class SearchProblem:
     def getSuccessors(self, state):
         """
           state: Search state
-
         For a given state, this should return a list of triples, (successor,
         action, stepCost), where 'successor' is a successor to the current
         state, 'action' is the action required to get there, and 'stepCost' is
@@ -58,7 +55,6 @@ class SearchProblem:
     def getCostOfActions(self, actions):
         """
          actions: A list of actions to take
-
         This method returns the total cost of a particular sequence of actions.
         The sequence must be composed of legal moves.
         """
@@ -78,13 +74,10 @@ def tinyMazeSearch(problem):
 def depthFirstSearch(problem):
     """
     Search the deepest nodes in the search tree first.
-
     Your search algorithm needs to return a list of actions that reaches the
     goal. Make sure to implement a graph search algorithm.
-
     To get started, you might want to try some of these simple commands to
     understand the search problem that is being passed in:
-
     print("Start:", problem.getStartState())
     print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
@@ -96,7 +89,6 @@ def depthFirstSearch(problem):
     while not stack.isEmpty():
         v = stack.pop()
         if problem.isGoalState(v[0]) :
-            print(v[1])
             return v[1]
         if v[0] not in discovered:
             discovered.append(v[0])
@@ -135,6 +127,20 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
+    q = util.PriorityQueue()
+    q.push((problem.getStartState(), []), 0)
+    cost = {}
+    cost[problem.getStartState()] = 0
+    while not q.isEmpty():
+        v = q.pop()
+        if problem.isGoalState(v[0]):
+            return v[1]
+        for w in problem.getSuccessors(v[0]):
+            newCost = cost[v[0]] + w[2]
+            if w[0] not in cost.keys() or newCost < cost[w[0]]:
+                cost[w[0]] = newCost
+                q.push((w[0], v[1]+[w[1]]), newCost + heuristic(w[0], problem))
+                
     util.raiseNotDefined()
 
 
